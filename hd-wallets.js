2ba02931cd2ad00cec5b34dd69c1ae9045bd0962
sajid-edninja <ssk73573@gmail.com>
2025-04-30 09:00:57 +0530

    create hd wallets

--- hd-wallets.js
+++ hd-wallets.js
@@ -5,3 +5,10 @@ import { Keypair } from "@solana/web3.js";
 
 const words = generateMnemonic(128);
 const seed = mnemonicToSeedSync(words);
+
+for (let i = 0; i < 4; i++) {
+  const path = `m/44'/501'/${i}'/0'`;
+  const derivedSeed = derivePath(path, seed.toString("hex")).key;
+  const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+  console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
+}
