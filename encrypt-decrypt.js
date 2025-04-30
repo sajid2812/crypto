c729c744b7d59d578a83d64b957bafd13c63ac84
sajid-edninja <ssk73573@gmail.com>
2025-04-30 07:36:49 +0530

    verify message

--- encrypt-decrypt.js
+++ encrypt-decrypt.js
@@ -12,6 +12,14 @@ async function main() {
 
   //sign the message
   const signature = await ed.signAsync(message, privateKey);
+
+  // verify the signature
+  try {
+    const isValid = await ed.verifyAsync(signature, "hello", publicKey);
+    console.log(isValid); // should print 'true' if the signature is valid
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 main();
