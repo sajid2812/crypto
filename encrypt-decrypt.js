6a83fb39193609a1b33fa63a9646f7fe7b861eba
sajid-edninja <ssk73573@gmail.com>
2025-04-30 07:36:34 +0530

    sign message

--- encrypt-decrypt.js
+++ encrypt-decrypt.js
@@ -3,6 +3,15 @@ import * as ed from "@noble/ed25519";
 async function main() {
   // Generate a secure random private key
   const privateKey = ed.utils.randomPrivateKey();
+
+  // Convert a message to Uint8Array
+  const message = new TextEncoder().encode("My name is Sk Sajid");
+
+  // Generate the public key from private key
+  const publicKey = await ed.getPublicKeyAsync(privateKey);
+
+  //sign the message
+  const signature = await ed.signAsync(message, privateKey);
 }
 
 main();
