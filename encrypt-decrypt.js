5f2a2530360eb57e493235f15e6c868b57525084
sajid-edninja <ssk73573@gmail.com>
2025-04-30 07:41:04 +0530

    fix code

--- encrypt-decrypt.js
+++ encrypt-decrypt.js
@@ -1,25 +1,12 @@
 import * as ed from "@noble/ed25519";
 
-async function main() {
-  // Generate a secure random private key
-  const privateKey = ed.utils.randomPrivateKey();
-
-  // Convert a message to Uint8Array
-  const message = new TextEncoder().encode("My name is Sk Sajid");
-
-  // Generate the public key from private key
-  const publicKey = await ed.getPublicKeyAsync(privateKey);
-
-  //sign the message
-  const signature = await ed.signAsync(message, privateKey);
-
-  // verify the signature
-  try {
-    const isValid = await ed.verifyAsync(signature, "hello", publicKey);
-    console.log(isValid); // should print 'true' if the signature is valid
-  } catch (e) {
-    console.log(e.message);
-  }
+const privKey = ed.utils.randomPrivateKey();
+const message = new TextEncoder().encode("sajid");
+const pubKey = await ed.getPublicKeyAsync(privKey);
+const sign = await ed.signAsync(message, privKey);
+try {
+  const isValid = await ed.verifyAsync(sign, message, pubKey);
+  console.log(isValid);
+} catch (e) {
+  console.log(e.message);
 }
-
-main();
